38875b2d0ea60121b223a3647bbda8c98a9ef638
salmon2073 <salmon2073@gmail.com>
2016-03-23 11:05:08 +0000

    Express_Startgit add .git add .

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	console.log("main.js");
+	
+	//resize();
+};
+
+resize(function(){
+	var width = window.innerWidth();
+	
+	console.log(width);
+    
+    var x = 500;
+    
+    if (width <= x) {
+    } else {
+    }
+});
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Expressにゃーーーん' });
-  res.sendFile('/home/ubuntu/workspace/grape/html/sushi.html');
+  res.sendFile('/home/ubuntu/workspace/grape/public/index.html');
 });
 
 module.exports = router;
