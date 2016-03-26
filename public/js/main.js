3f71eb2ac8a689114cfb241508c82425d5f3b80e
pvcresin <metal.gear.solid.portable.ops.plus@gmail.com>
2016-03-26 17:46:46 +0000

    scroll search box main page

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,41 @@
-//resize();   // resize first
+var headerPos = 0, previous_scroll_pos = 0;
 
 window.onload = function() {
     resize();
     
-    var screenWidth = screen.width;
-    var width = document.body.clientWidth;
+    var screenWidth = screen.width, width = document.body.clientWidth;
     //alert("screenW : " + screenWidth + ", width : " + width);
 
     debugColor();
 
     setImgListener(); // post-picture-image -> click
+    
+    setSearchListener();
+    
+    setReactListener();
+    
+};
+
+function setReactListener() {
+    var replyBox = document.getElementsByClassName("react-reply-box");
+    var shareBox = document.getElementsByClassName("react-share-box");
+    var likeBox = document.getElementsByClassName("react-like-box");
+    
+    var replyNum = document.getElementsByClassName("react-reply-num");
+    var shareNum = document.getElementsByClassName("react-share-num");
+    var likeNum = document.getElementsByClassName("react-like-num");
+    
+    for (var i = 0; i < replyBox.length; i++) {
+        replyBox[i].addEventListener("click", function(event){
+            var str = replyNum[0].innerHTML;
+            var nb = parseInt(str.trim(), 10);
+            replyNum[0].innerHTML = ++nb;
+        }, false);
+    }
+}
+
+window.onscroll = function(){
+    locateHeader();
 };
 
 window.onresize = function() {
@@ -54,6 +80,38 @@ function resize() {
     }
 }
 
+function locateHeader() {
+    var current_pos = document.documentElement.scrollTop || document.body.scrollTop;
+    var dif_pos = current_pos - previous_scroll_pos;
+    
+    if (dif_pos > 0) setHeaderPos(headerPos - dif_pos);    // down -> hide header
+    else setHeaderPos(headerPos - dif_pos);    // up -> appear header
+    previous_scroll_pos = current_pos;
+    
+    function setHeaderPos(posY) {
+        var header = document.getElementById("header"), headerH = header.clientHeight;
+        var headerBG = document.getElementById("headerBG");
+        
+        if (posY < -headerH) posY = -headerH;   // over up
+        if (posY > 0) posY = 0; // over down
+        
+        header.style.top = posY + "px";
+        headerBG.style.height = headerH + posY + "px";
+        
+        headerPos = posY;
+    }
+}
+
+function setSearchListener() {
+    var searchBox = document.getElementById("searchBox");
+    
+    searchBox.addEventListener("keyup", function(event) {
+        var text = event.target.value;
+        if (event.keyCode == 13) console.log("Enter -> search: " + text);
+        else console.log(text);
+    }, false);
+}
+
 function setImgListener() {
     var imgs = document.getElementsByClassName("post-picture");
 
@@ -64,6 +122,7 @@ function setImgListener() {
         }, false);
     }
 }
+
 
 function debugColor(){
     
