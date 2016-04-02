b590af7b0197d461851440050beeb016f82dad0d
pvcresin <metal.gear.solid.portable.ops.plus@gmail.com>
2016-04-02 14:36:54 +0000

    main page: color mode, resizable search

--- public/js/main.js
+++ public/js/main.js
@@ -2,54 +2,20 @@ var headerPos = 0, previous_scroll_pos = 0;
 var request = window.superagent;
 
 window.onload = function() {
+    setColorMode("light");  // debug, light
+    
     resize();
     
-    var screenWidth = screen.width, width = document.body.clientWidth;
-    //alert("screenW : " + screenWidth + ", width : " + width);
-
-    debugColor();
-
-    setImgListener(); // post-picture-image -> click
+    changePostTime("relative");
     
     setSearchListener();
-    
-    setReactListener();
-    
     setPostListener();
-    
+    setImgListener();
+    setReactListener();
 };
-function setReactListener() {
-    var replyBox = document.getElementsByClassName("react-reply-box");
-    var shareBox = document.getElementsByClassName("react-share-box");
-    var likeBox = document.getElementsByClassName("react-like-box");
-    
-    for (var i = 0; i < replyBox.length; i++) {
-        replyBox[i].addEventListener("click", function(event){
-            console.log(event);
-            console.log(this);
-            var str = this.childNodes[3].innerHTML; // p tag
-            var num = parseInt(str.trim(), 10);
-            this.childNodes[3].innerHTML = ++num;
-        }, false);
-        shareBox[i].addEventListener("click", function(event){
-            console.log(event);
-            console.log(this);
-            var str = this.childNodes[3].innerHTML; // p tag
-            var num = parseInt(str.trim(), 10);
-            this.childNodes[3].innerHTML = ++num;
-        }, false);
-        likeBox[i].addEventListener("click", function(event){
-            console.log(event);
-            console.log(this);
-            var str = this.childNodes[3].innerHTML; // p tag
-            var num = parseInt(str.trim(), 10);
-            this.childNodes[3].innerHTML = ++num;
-        }, false);
-    }
-}
 
-window.onscroll = function(){
-    locateHeader();
+window.onscroll = function() {
+    // locateHeader();
 };
 
 window.onresize = function() {
@@ -60,6 +26,8 @@ function resize() {
     var screenWidth = screen.availWidth,
         screenHeight = screen.availHeight;
     var width = document.body.clientWidth;
+    
+    //console.log("screenW : " + screenWidth + ", width : " + width);
 
     if (screenWidth <= 600) resizeContent("0%", "100%", "0%"); // smartphone
     else if (screenWidth <= 800) resizeContent("10%", "80%", "10%"); // tablet
@@ -75,23 +43,75 @@ function resize() {
             resizeHeader(sideW * 2 / 3 + "px");
         }
     }
-
+    
+    resizeSearch();
+    
     function resizeContent(leftSize, centerSize, rightSize) {
-        var leftBox = document.getElementById("content-left");
-        leftBox.style.width = leftSize;
-
-        var centerBox = document.getElementById("content-center");
-        centerBox.style.width = centerSize;
-
-        var rightBox = document.getElementById("content-right");
-        rightBox.style.width = rightSize;
+        document.getElementById("content-left").style.width = leftSize;
+        document.getElementById("content-center").style.width = centerSize;
+        document.getElementById("content-right").style.width = rightSize;
     };
-
     function resizeHeader(padSide) {
         var headerContent = document.getElementById("header-content");
         headerContent.style.paddingLeft = padSide;
         headerContent.style.paddingRight = padSide;
     }
+    function resizeSearch(){
+        var searchBoxW = document.getElementById("search-box").offsetWidth;
+        var imgW = document.getElementById("search-box-img").offsetWidth;
+        var input = document.getElementById("search-box-input");
+        input.style.width = (searchBoxW - 2.3 * imgW) + "px";
+    }
+}
+
+function setColorMode(mode){
+    if (mode === "debug"){
+        tagBgColor("body", "#999999");
+        
+        idBgColor("header", "red");
+        idBgColor("headerBG", "#00ff00");
+        idBgColor("logo", " #aa00aa");
+        idBgColor("search", "#555555");
+        idBgColor("search-box-input", "magenta");
+        idBgColor("buttons", "#0000aa");
+        idBgColor("buttons-inner", "yellow");
+        
+        idBgColor("content-left", "#eeeeee");
+        idBgColor("content-center", "#ddddee");
+        idBgColor("content-right", "#eeeeee");
+        
+        idBgColor("post-box", "purple");
+        
+        classBgColor("post", "white");
+        classBgColor("post-header", "#ffff00");
+        classBgColor("post-content", "#ffffff");
+        classBgColor("post-text", "gray");
+        classBgColor("post-footer", "brown");
+        classBgColor("react-area", "yellow");
+        classBgColor("react-box", "lightGray");
+        
+        idBgColor("footer", "magenta");
+        
+    } else if (mode === "light") {
+        tagBgColor("body", "#F5F5F5");
+        // idBgColor("header", "purple");
+        idBgColor("header", "white");
+        idBgColor("post-box", "white");
+        classBgColor("post", "white");
+    }
+    
+    function idBgColor(id, color) {
+        document.getElementById(id).style.backgroundColor = color;
+    }
+    function classBgColor(className, color) {
+        var classes = document.getElementsByClassName(className);
+        for (var i = 0; i < classes.length; i++) classes[i].style.backgroundColor = color;
+    }
+    function tagBgColor(tag, color) {
+        var tags = document.getElementsByTagName(tag);
+        for (var i = 0; i < tags.length; i++) tags[i].style.backgroundColor = color;
+    }
+    
 }
 
 function locateHeader() {
@@ -126,6 +146,28 @@ function setSearchListener() {
     }, false);
 }
 
+function changePostTime(mode) { // vanish someday & server side process
+    if (mode === "relative") {
+        var times = document.getElementsByClassName("post-time");
+        console.log(times);
+        
+        for (var i = 0; i < times.length; i++) {
+            var t = times[i].innerHTML.split("-");
+            var year = Number(t[0]), month = Number(t[1]), date = Number(t[2]);
+            var hour = Number(t[3]), minut = Number(t[4]), second = Number(t[5]);
+            
+            var current = getTime();
+        }
+    }
+    
+    function getTime() {        // month: 0, 1, 2..
+        var time = new Date();
+        var year = time.getFullYear(), month = time.getMonth() + 1, date = time.getDate();
+        var hour = time.getHours(), minute = time.getMinutes(), second = time.getSeconds();
+        return [year, month, date, hour, minute, second];
+    }
+}
+
 function setImgListener() {
     var imgs = document.getElementsByClassName("post-picture");
 
@@ -133,6 +175,36 @@ function setImgListener() {
         imgs[i].addEventListener('click', function() {
             var attr = this.getAttribute("src");
             console.log("image click, src : " + attr);
+        }, false);
+    }
+}
+
+function setReactListener() {
+    var replyBox = document.getElementsByClassName("react-reply-box");
+    var shareBox = document.getElementsByClassName("react-share-box");
+    var likeBox = document.getElementsByClassName("react-like-box");
+    
+    for (var i = 0; i < replyBox.length; i++) {
+        replyBox[i].addEventListener("click", function(event){
+            // console.log(event);
+            // console.log(this);
+            var str = this.childNodes[3].innerHTML; // p tag
+            var num = parseInt(str.trim(), 10);
+            this.childNodes[3].innerHTML = ++num;
+        }, false);
+        shareBox[i].addEventListener("click", function(event){
+            // console.log(event);
+            // console.log(this);
+            var str = this.childNodes[3].innerHTML; // p tag
+            var num = parseInt(str.trim(), 10);
+            this.childNodes[3].innerHTML = ++num;
+        }, false);
+        likeBox[i].addEventListener("click", function(event){
+            // console.log(event);
+            // console.log(this);
+            var str = this.childNodes[3].innerHTML; // p tag
+            var num = parseInt(str.trim(), 10);
+            this.childNodes[3].innerHTML = ++num;
         }, false);
     }
 }
@@ -160,7 +232,7 @@ function setPostListener(){
         var minute = date.getMinutes();
         var second = date.getSeconds();
         
-        var postTime = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+        var postTime = year + "-" + month + "-" + day + "-" + hour + "-" + minute + "-" + second;
         
         var postArea = document.getElementById("post-area");
         var post = heredoc({
@@ -201,10 +273,6 @@ function setPostListener(){
     })
 }
 
-
-function debugColor(){
-    
-}
 
 function heredoc(data,func){
     var _data = null, _func = null;
